aef11dd6c252254acf439c96f566afdd6618597f
Silletr <hristicenkoigor764@gmail.com>
2026-03-22 12:18:14 +0200

    [CHANGED FILE/DIR: src/ahk2python/parser.py] Added basic map system (for now - working only ::UrText:OnWhatReplace - will be added more in future). Returning to Ahk-auto-switch xD [TIMESTAMPl 22/03/2026 | 12:18]

--- src/ahk2python/parser.py
+++ src/ahk2python/parser.py
@@ -1,2 +1,44 @@
 from dataclasses import dataclass
 from enum import Enum
+
+
+class LineType(Enum):
+    HOTSTRING = "hotstring"  # ::ae:ä
+    HOTKEY = "hotkey"  # ^j::Send, text
+    COMMENT = "comment"  # ; <- comment
+    EMPTY = "empty"
+    UNKNOWN = "unknown"
+
+
+@dataclass
+class AHKLine:
+    raw: str
+    line_type: LineType
+    data: dict  # Aready parsed values
+
+
+def parse_hotkeys(line: str) -> tuple[str, str]:
+    """Parse hotstring into trigger and replacement."""
+    rest = line[2:]  # remove opening ::
+    parts = rest.split(":", 1)  # split on FIRST colon only
+    trigger = parts[0]
+    replacement = parts[1].lstrip(":")  # remove extra : if ::
+    return trigger, replacement
+
+
+def classify_line(line: str) -> AHKLine:
+    line = line.strip()
+
+    if not line:
+        return AHKLine(raw=line, line_type=LineType.EMPTY, data={})
+    if line.startswith(";"):
+        return AHKLine(raw=line, line_type=LineType.COMMENT, data={})
+
+    if line.startswith("::") and ":" in line[2:]:
+        return AHKLine(raw=line, line_type=LineType.HOTSTRING, data={})
+
+    return AHKLine(raw=line, line_type=LineType.UNKNOWN, data={})
+
+
+print(parse_hotkeys("::ae:ä"))
+print(parse_hotkeys("::ae::ä"))
